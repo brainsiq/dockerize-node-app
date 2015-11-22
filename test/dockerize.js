a15b640bf6bffb5b920dfe710b102d460af4e7b4
chrisbrainsick <chrisbrainsick@gmail.com>
2015-11-22 12:41:37 +0000

    add CMD to dockerfile

--- test/dockerize.js
+++ test/dockerize.js
@@ -30,6 +30,19 @@ describe('Dockerize', () => {
       nock.cleanAll();
     });
 
+    it('throws when there is no package.json', done => {
+      return dockerize.dockerfile(__dirname + '/12345')
+        .then(() => {
+          done(new Error('Expected test to error'));
+        })
+        .catch(err => {
+          expect(err).to.exist;
+          expect(err).to.be.a('error');
+          expect(err.message).to.equal(`${__dirname}/12345 does not contain a package.json file!`);
+          done();
+        });
+    });
+
     describe('node version from .nvmrc', () => {
       it('requests node version from semver.io', () => {
         return dockerize.dockerfile(__dirname + '/sample_app_nvmrc')
@@ -100,6 +113,37 @@ describe('Dockerize', () => {
             expect(err).to.exist;
             expect(err).to.be.a('error');
             expect(err.message).to.equal('Unable to get node version for base image from https://semver.io. 500 - an error from semver.io');
+            done();
+          });
+      });
+    });
+
+    describe('CMD', () => {
+      it('is npm start when defined in package.json', () => {
+        return dockerize.dockerfile(__dirname + '/sample_app_npm_start')
+          .then(dockerfile => {
+            const lines = dockerfile.split(os.EOL);
+            expect(lines[lines.length - 1]).to.equal('CMD ["npm", "start"]');
+          });
+      });
+
+      it('is node [entrypoint] when defined in package.json', () => {
+        return dockerize.dockerfile(__dirname + '/sample_app_node_entrypoint')
+          .then(dockerfile => {
+            const lines = dockerfile.split(os.EOL);
+            expect(lines[lines.length - 1]).to.equal('CMD ["node", "entrypoint.js"]');
+          });
+      });
+
+      it('throws when unknown', done => {
+        return dockerize.dockerfile(__dirname + '/sample_app_unknown_cmd')
+          .then(dockerfile => {
+            done(new Error('Expected an error in test'));
+          })
+          .catch(err => {
+            expect(err).to.exist;
+            expect(err).to.be.a('error');
+            expect(err.message).to.equal('Cannot determine CMD for Dockerfile. Set "start" script or "main" attribute in package.json!');
             done();
           });
       });
