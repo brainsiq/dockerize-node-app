4d0f51174bbdaf5f1852b8b5abd1d64a9586ff79
chrisbrainsick <chrisbrainsick@gmail.com>
2015-11-29 17:17:02 +0000

    build image and run container automatically

--- index.js
+++ index.js
@@ -3,22 +3,65 @@
 'use strict';
 
 const fs = require('fs');
+const exec = require('child_process').exec;
+
 const dockerize = require('./lib/dockerize');
+const commandBuilder = require('./lib/command_builder');
 
 const directory = process.cwd();
 const dockerfileLocation = `${directory}/Dockerfile`
 
-dockerize.dockerfile(process.cwd())
-  .then(dockerfile => {
-    fs.writeFile(dockerfileLocation, dockerfile, err => {
-      if (err) {
-        console.error(`Unable to write dockerfile to ${dockerfileLocation}.`);
-        return console.error(err);
+const executeCommand = (command, directory) => {
+  return new Promise((resolve, reject) => {
+    const proc = exec(command, {cwd: directory});
+
+    proc.stdout.pipe(process.stdout);
+
+    proc.stderr.pipe(process.stderr);
+
+    proc.on('exit', code => {
+      if (code === 0) {
+        return resolve();
       }
 
-      console.log('Created Dockerfile');
+      const error = new Error('command failed');
+      error.exitCode = code;
+
+      reject(err);
     });
   })
-  .catch(err => {
-    console.error(err);
-  });
+};
+
+dockerize.dockerfile(directory)
+  .then(dockerfile => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(dockerfileLocation, dockerfile, err => {
+        if (err) {
+          console.error(`Unable to write dockerfile to ${dockerfileLocation}.`);
+          reject(err);
+        }
+
+        console.log('Created Dockerfile');
+        resolve();
+      });
+    });
+
+  })
+  .then(() => {
+    const command = commandBuilder.dockerBuild(directory);
+
+    console.log(`Building docker image with command: ${command}`);
+
+    return executeCommand(command, directory);
+  })
+  .then(() => {
+    console.log('Built docker image');
+
+    const command = commandBuilder.dockerRun(directory);
+
+    console.log(`Running docker image with command: ${command}`);
+
+    return executeCommand(command, directory);
+  }, err => { throw new Error(`docker build command failed with exit code ${err.code}`); })
+  .then(() => console.log('Exited container'), err => { throw new Error(`docker run command failed with exit code ${err.code}`) })
+  .catch(console.error);
---
+++ test/command_builder.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const commandBuilder = require('../lib/command_builder');
+
+describe('Command builder', () => {
+  const directory = __dirname + '/sample_app_source_code';
+
+  describe('docker build', () => {
+    let command;
+
+    before(() => {
+      command = commandBuilder.dockerBuild(directory);
+    });
+
+    it('creates a docker build command', () => {
+      expect(command).to.match(/^docker build /);
+    });
+
+    it('uses [app-name] as the image name', () => {
+      expect(command).to.match(/ -t package-json-name/);
+    });
+
+    it('builds from the given directory', () => {
+      expect(command).to.match(new RegExp(` ${directory}$`));
+    });
+  });
+
+  describe('docker run', () => {
+    let command;
+
+    before(() => {
+      command = commandBuilder.dockerRun(directory);
+    });
+
+    it('creates a docker run command', () => {
+      expect(command).to.match(/^docker run /);
+    });
+
+    it('runs the [app-name] image', () => {
+      expect(command).to.match(/ package-json-name$/);
+    });
+
+    it('runs a temporary container', () => {
+      expect(command).to.match(/ --rm /);
+    });
+  });
+});
